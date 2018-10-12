dcad5cad14bb558b7b30234880e6bfc576f9fa3d
Yazdan <ykakaei.psy@gmail.com>
2018-10-12 22:07:54 +0200

    final touch-ups

--- script.js
+++ script.js
@@ -1,20 +1,26 @@
 //Listen for submit
 
-document.getElementById('loan-form').addEventListener('submit', calculateResults);
+document.getElementById("loan-form").addEventListener("submit", function(e) {
+  //Hide results
+  document.getElementById("results").style.display = "none";
+  //Show loader
+  document.getElementById("loading").style.display = "block";
+
+  setTimeout(calculateResults, 2000);
+  e.preventDefault();
+});
 
 //Calculate the results
-function calculateResults(e) {
-
-  console.log('Calculating...');
+function calculateResults() {
+  console.log("Calculating...");
 
   //UI variables
-  const amount = document.getElementById('amount');
-  const interest = document.getElementById('interest');
-  const years = document.getElementById('years');
-  const monthlyPayment = document.getElementById('monthly-payment');
-  const totalPayment = document.getElementById('total-payment');
-  const totalInterest = document.getElementById('total-interest');
-
+  const amount = document.getElementById("amount");
+  const interest = document.getElementById("interest");
+  const years = document.getElementById("years");
+  const monthlyPayment = document.getElementById("monthly-payment");
+  const totalPayment = document.getElementById("total-payment");
+  const totalInterest = document.getElementById("total-interest");
 
   const principle = parseFloat(amount.value);
   const calculatedInterest = parseFloat(interest.value) / 100 / 12;
@@ -27,47 +33,46 @@ function calculateResults(e) {
   if (isFinite(monthly)) {
     monthlyPayment.value = monthly.toFixed(2);
     totalPayment.value = (monthly * calculatedPayments).toFixed(2);
-    totalInterest.value = ((monthly * calculatedPayments) - principle).toFixed(2);
+    totalInterest.value = (monthly * calculatedPayments - principle).toFixed(2);
 
+    //Show the results
+    document.getElementById("results").style.display = "block";
+
+    //Hide loader
+    document.getElementById("loading").style.display = "none";
   } else {
-    showError('Please Check Your Numbers');
+    showError("Please Check Your Numbers");
   }
-
-  e.preventDefault();
-
 }
-
 
 // Show Error
 
 function showError(error) {
-
+  //Hide results
+  document.getElementById("results").style.display = "none";
+  //Show loader
+  document.getElementById("loading").style.display = "none";
   //Create a div
-  const errorDiv = document.createElement('div');
-
+  const errorDiv = document.createElement("div");
 
   //Get elements
-  const card = document.querySelector('.card');
-  const heading = document.querySelector('.heading');
+  const card = document.querySelector(".card");
+  const heading = document.querySelector(".heading");
 
-
-  //Add class 
-  errorDiv.className = 'alert alert-danger';
+  //Add class
+  errorDiv.className = "alert alert-danger";
 
   //Create text node
   errorDiv.appendChild(document.createTextNode(error));
-
 
   //Insert Error Above Heading
   card.insertBefore(errorDiv, heading);
 
   //Clear Error after 3 seconds
   setTimeout(clearError, 3000);
-
 }
-
 
 //Clear Error
 function clearError() {
-  document.querySelector('.alert').remove();
+  document.querySelector(".alert").remove();
 }
